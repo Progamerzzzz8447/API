35a8c1915efb8bd1e547f0d4e8ecbdd23e490b01
Progamerzzzz8447 <JH2010DH2013@HOTMAIL.COM>
2025-05-16 06:54:34 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const storedData = [];
+
+app.post('/data', (req, res) => {
+  const value = req.body.value;
+  if (value) {
+    storedData.push(value);
+    res.status(200).json({ message: 'Value stored', data: storedData });
+  } else {
+    res.status(400).json({ message: 'No value provided' });
+  }
+});
+
+app.get('/data', (req, res) => {
+  res.json({ data: storedData });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
